7959b7b7f6baa2a7a0ac9c58dd1f68d5cfa460ae
John Gibbons <jvgiv12@gmail.com>
2019-07-01 20:24:52 -0400

    string reversed

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,20 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    // return str.split('').reverse().join('')
+    // or
+    // let reversed = ''
+
+    // for (let character of str) {
+    //     reversed = character + reversed;
+    // }
+
+    // return reversed
+    // or 
+    return str.split('').reduce((reversed, character) => {
+        return character + reversed
+    }, '')
+}
 
 module.exports = reverse;
